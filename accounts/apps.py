fc21ef1f239c585e6937b6db4b1e1cea2ee8c525
alchemist1111 <emoyoeugine@gmail.com>
2025-07-23 18:11:58 +0300

    Registered signals

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "accounts"
+    
+    
+    def ready(self):
+        # Import signals to ensure they are registered
+        import accounts.signals
+        # Ensure signals are imported when the app is ready
+        print("Accounts app is ready and signals are imported.")
