9d465af37d0c1ca332dfa8aafb1678d5aae9d46a
alchemist1111 <emoyoeugine@gmail.com>
2025-07-24 10:31:22 +0300

    created the orders table and added its attributes

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+
+# Class to represent an order
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Processing', 'Processing'),
+        ('Shipped', 'Shipped'),
+        ('Delivered', 'Delivered'),
+        ('Cancelled', 'Cancelled'),
+    ]
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='orders')
+    order_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Pending')
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"Order {self.id} by {self.user.email} - {self.status}"
