b26423eabb824464d9dca7a1d68ba3da8baf53d9
alchemist1111 <emoyoeugine@gmail.com>
2025-07-24 22:40:35 +0300

    Registered category and products model in admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+# Category model registration with admin site
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+# Products model registration with admin site
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'created_at', 'updated_at')
+    search_fields = ('name', 'description')
+    list_filter = ('category', 'created_at', 'updated_at')
