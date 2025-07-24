40eb835674a5c26a1ba77d461557c05fbd654465
alchemist1111 <emoyoeugine@gmail.com>
2025-07-24 22:31:37 +0300

    Registered shipping model in admin

--- shipping/admin.py
+++ shipping/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Shipping
 
-# Register your models here.
+
+# Register shipping model with the admin site
+admin.site.register(Shipping)
+class ShippingAdmin(admin.ModelAdmin):
+    list_display = ('order', 'shipping_address', 'tracking_number', 'status', 'shipping_date')
+    search_fields = ('order__id', 'tracking_number', 'shipping_address')
+    list_filter = ('status', 'shipping_date')
