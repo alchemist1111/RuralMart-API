567621a3f85ba7c0195e98ac05aa9e719fb7899e
alchemist1111 <emoyoeugine@gmail.com>
2025-07-24 22:37:53 +0300

    Registered orders model in admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+# Order model registration with the admin site
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'order_date', 'status', 'total_price')
+    search_fields = ('user__username', 'status')
+    list_filter = ('status', 'order_date')
+
+# OrderItem model registration with the admin site
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity', 'unit_price')
+    search_fields = ('order__id', 'product__name')
+    list_filter = ('order',)
