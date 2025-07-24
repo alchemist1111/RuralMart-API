963d400a24ec56f90bce5e12da6a48df00fafa28
alchemist1111 <emoyoeugine@gmail.com>
2025-07-24 22:35:40 +0300

    Registered payments model in admin

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+# Register payment model with the admin site
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('order', 'payment_date', 'payment_method', 'amount', 'status')
+    search_fields = ('order__id', 'payment_method')
+    list_filter = ('status', 'payment_method', 'payment_date')
