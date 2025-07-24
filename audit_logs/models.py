9a7c5e1c3acf03bcae59d7da7c05c0b1ca867aad
alchemist1111 <emoyoeugine@gmail.com>
2025-07-24 11:03:07 +0300

    Created audit logs table and added its attributes

--- audit_logs/models.py
+++ audit_logs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+# Class to represent audit logs
+class AuditLog(models.Model):
+    ACTION_CHOICES = [
+        ('create', 'Create'),
+        ('update', 'Update'),
+        ('delete', 'Delete'),
+        ('view', 'View'),
+    ]
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='audit_logs')
+    action = models.CharField(max_length=255, choices=ACTION_CHOICES)
+    model_name = models.CharField(max_length=255)
+    model_id = models.PositiveIntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name} - {self.action} - {self.model_name} (ID: {self.model_id}) at {self.timestamp}"
