0d8a58b987cc4c752197a0a72c6cc57e97a341df
alchemist1111 <emoyoeugine@gmail.com>
2025-07-24 10:35:18 +0300

    created the order items table and added its attributes

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import CustomUser
+from products.models import Product
 
 
 # Class to represent an order
@@ -18,3 +19,15 @@ class Order(models.Model):
     
     def __str__(self):
         return f"Order {self.id} by {self.user.email} - {self.status}"
+    
+    
+# Class to represent an order item
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order {self.order.id}"    
