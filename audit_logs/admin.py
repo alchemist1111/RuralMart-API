3b27252e8c6003625f90ccf8b0924fd6504cd91d
alchemist1111 <emoyoeugine@gmail.com>
2025-07-24 22:28:40 +0300

    Registered audit logs model in admin

--- audit_logs/admin.py
+++ audit_logs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import AuditLog
 
-# Register your models here.
+# Register the AuditLog model with the admin site
+@admin.register(AuditLog)
+class AuditLogAdmin(admin.ModelAdmin):
+    list_display = ('timestamp', 'user', 'action', 'object_id', 'object_repr')
+    search_fields = ('user__first_name', 'action', 'object_repr')
+    list_filter = ('action', 'timestamp')
+    ordering = ('-timestamp',)
+    date_hierarchy = 'timestamp'
