4239f80967bf9fefd0fec85050d7133708727fbf
alchemist1111 <emoyoeugine@gmail.com>
2025-07-24 10:46:30 +0300

    Created payments table and added its attributes

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from orders.models import Order
 
-# Create your models here.
+# Class to represent a payment
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ('Credit Card', 'Credit Card'),
+        ('Debit Card', 'Debit Card'),
+        ('MPesa', 'M-Pesa'),
+        ('Cash', 'Cash'),
+    ]
+    
+    PAYMENT_STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Failed', 'Failed'),
+        ('Refunded', 'Refunded'),
+    ]
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='payment')
+    payment_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=50, choices=PAYMENT_METHOD_CHOICES, default='Credit Card')
+    payment_status = models.CharField(max_length=50, choices=PAYMENT_STATUS_CHOICES, default='Pending')
+    payment_date = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return f"Payment for Order {self.order.id} - {self.payment_status} ({self.payment_method})"
