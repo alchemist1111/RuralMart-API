13ea2e8f92aefd74e4fd559bc9fc0d087ff3a1a5
alchemist1111 <emoyoeugine@gmail.com>
2025-07-24 10:06:58 +0300

    Added the products and categories tables plus their respective attributes

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Product model to represent products in the rural mart
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    Category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='products')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.name} - {self.price}"
+    
+    
+    
+# Category model to represent product categories
+class Category(models.Model):
+    name = models.CharField(max_length=100) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
