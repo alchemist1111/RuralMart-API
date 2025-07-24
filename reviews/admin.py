a1d760577cb6bbcbb0b508bd093a029fc338d11d
alchemist1111 <emoyoeugine@gmail.com>
2025-07-24 22:33:22 +0300

    Registered reviews model in admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+# Register review model with the admin site
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'rating', 'created_at')
+    search_fields = ('user__username', 'product__name', 'comment')
+    list_filter = ('rating', 'created_at')
